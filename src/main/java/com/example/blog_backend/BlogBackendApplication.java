f0ce2c16d87d0cfdd782e4e26c50f29f3667d8dc
deepakSingh023 <dsinghrana71@gmail.com>
2026-02-14 04:19:26 +0530

    added scheduling and task to keep service alive

--- src/main/java/com/example/blog_backend/BlogBackendApplication.java
+++ src/main/java/com/example/blog_backend/BlogBackendApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-
+@EnableScheduling
 @EnableRetry
 @EnableAsync
 @SpringBootApplication
