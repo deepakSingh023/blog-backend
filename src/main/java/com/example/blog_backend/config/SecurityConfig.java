a61e98c0252ffa2619f1c85b00a87c401697e7b9
deepakSingh023 <dsinghrana71@gmail.com>
2026-02-14 04:44:07 +0530

    forgot to add the deployment link of frontend in the cors

--- src/main/java/com/example/blog_backend/config/SecurityConfig.java
+++ src/main/java/com/example/blog_backend/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("*"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization"));
